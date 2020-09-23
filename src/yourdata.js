fa98c5f354c01985f765ee9f554737de1cb497ce
Aresky Berkane <areskyberkane@gmail.com>
2020-09-23 19:09:05 +0200

    change image path

--- src/yourdata.js
+++ src/yourdata.js
@@ -1,3 +1,9 @@
+import profile from "./files/profile.jpg";
+import bag from "./files/bag.png";
+import farm from "./files/farm.png";
+import kanban from "./files/kanban.png";
+import pi from "./files/1000pi.png";
+
 export default {
   //(Please Do Not Remove The comma(,) after every variable)
   //Change The Website Template
@@ -15,8 +21,7 @@ export default {
   // Add Your About Text Here
   abouttext:
     "After graduating with a Higher National Diploma in Software and working within a hospitality organisation for 2 years, I made the decision to pursue a career within Technology. This fashination started long before my GCE in Science, after considering my options with the hospitality sector I realised Technology could provide me with so many more opportunities to grow. Bringing my growth mindset and a love of problem solving.",
-  aboutImage:
-    "https://lh3.googleusercontent.com/qqCYIB_TVmPopPPwjkqjheKE0aYD961wdtzOt2s-lGRD-wX3ItjIaUZafpyDy7T_1sLKdG-1aP7WDKYEtUPrr2BPq1hUKg7yZFADHrK5qq62l215Za8MdFokdsBN_LJRioVpzMs3IEexuw5AsPPstI2mPMhFs6zC72WPnu59iaS4Hhu5KudxOypBwZGfB6s_dVdP5shA_y7evTHJ-O7JPRvJx1ezohAR7Kj09mm56IVUSdEe31mtdYGjTsN50zOgnDQhiCJE8XogKivUy5lWC3qf2WN0c2VNdh8A0hMa2nuE2soJyEmHfWX58JeY59zTIwXggB8uvQLdQY4z-7DVPNIoL2RsV323fjBn48zh4eezFU9yL9eR68s1_Rlrk8RVjTm3V4-7PjgJj_2WPTW9QP22f1G9h0cB8rPRLBldXH2mGtMn-qWMl_aqK2SD-Eax6zJ3uuQb6xNbQAkImVO6vVszS2IAfOf2DTAORkB-DwS45lJO1KgSnBTN1RfMltASuia8a0wsuhWh8w6ciZ_gebeQngQyaG9WXv3KgLiZHdHNOtmb6lrK2R5i1Ay5CBk-e14LUZPz241md2Rq4-nZTosTOo2ZCNdG2dneQg12eSdXZNXqR1fTKJp1dCSOs2gP6BOnt1xbvDK1nFtI-RsZAJfAEPegNcoKqw6oCbr8oGReD9Fu2N5xPgsoRWRm=w500-h700-no?authuser=0",
+  aboutImage: profile,
   //Change This To Hide The Image of About Section (True Or False)
   ShowAboutImage: true, //true or false (Change Here)ß
   // Change Projects Here
@@ -26,8 +31,7 @@ export default {
       title: "1000π", //Project Title - Add Your Project Title Here
       service: "Web Teaching Platform (React.js, Firebase)", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
-      imageSrc:
-        "https://lh3.googleusercontent.com/s2ZcM4k7VQXPoeD26EJPiWPn2jqYh3m3X8F-HN_xjZ7FO14PXMhbRE6Gz-7m86MypikomVmEYK5la0acdGNV3kNOHSJVhfQWr4ugCbGKs9FfFhqhwYCKF1qkQCkYM6KgHL0xKNQGElp9-qmtGRUkaqfTO-bN-coydUOA0J6NT0KAO_LWoH9luhfBAn53Mb5GAABB2So-GIWAL_QlVBU0vE7Qd7XEEAExLOVG1M26SNot6KYSNxR3axtg8CLAqiccYnwv3fW1Rjm_fnEQ_gTd5RkncAOW77IbRjth-6D3huCO_XcGI5NgdzHvs6yTIVj18DVQ6qkVzMDkMV-4agObs1qKbYj0q4qd04OynVhuN3z46-Eq9zSX9srtHuOR1Q7sMD106j0t17rm3maDA1JSuK9GfZU72wBhn67qmihZLb5alMqzUsyIUuRvE5elJzHHFvAmw69TEyOTVCvc45_RgBwNCtZxoi-drKrjIdIUUq71ze4Og5rdwWR753KI_DmOxQqaG7k_8N7hiJAdtCVrX1vapTCvMyo3GrhzVdgKsOqK4JxlYZ8HxixZWhDtYgYiXAsHL63GtQt2c5qns-ycacTYup097tWhBHsfQ5npv2DxOtHQr2xncYVIAYUQ_QpVciChao0ehteKtetk1mPKlhWTltTfb5937zgPHLaZiW26j7hBCoXb5tWC1A9R=s609-no?authuser=0",
+      imageSrc: pi,
       //Project URL - Add Your Project Url Here
       url: "https://pi-4a942.firebaseapp.com/",
     },
@@ -35,24 +39,21 @@ export default {
       id: 2, //DO NOT CHANGE THIS (Please)😅
       title: "Virtual Garden",
       service: "Web Game, HND Group Project (Vue.js, Java Spring)",
-      imageSrc:
-        "https://lh3.googleusercontent.com/phy2wrPBG-cTEoo4VydwNHo4rtkFMOIUu3d9U0r9_yq5oOtyScgYxGEbAs6q3ltM3qIEICcKTgqnD9t4Hd_0fzDQacTvPFQBO7aF3WEDZ_SNsPcJ_V43A-NwrCNsM3isDRULeVJwA7hFShpDJcVXmmBY9c0Sv5-4fbvub_TskHN_aPKK9okysN7W3FFeU8SdxLfeWkBElcP-c7Ok_b0x5gdERaGUIrzz_YHFTJK8DMAH-Yz_qgx41KaeQqNm-2FaXBKk2maSUzZCSuNdEnqwwjOJgOWlJOtWGubdUkVO9Q1VstjAJC9v5cySwz-3hNpOszfmOwo-07Y4y6qcGTSFOAS2BUmtt-DqmVpAekVrIBZOZPzmcHocxNxI6uAPz1Ljn27wFdFIE6xrDPnJEr9SBWad4_BQIcEAEEhyTdpTO4gkfNVfkvxKxy_tSUIwE99WtZsFZBBJ_KcscfgVTTb9B2fifQe-qP4J3GfTTI5qe2GUh0aRvy_yA6dSztyWKKnQwFpDeQx648xGA1ma3ESxuUrdHZm5PB6RY6zi1pLx0cUlf0UnSyzlsJMIosfq3O7zxVI7y7OI4f_1RrcOmQLXR5xFmaxxXKP60TRcu6PgCuiuBPs5KWE8sZAWH-sdF7ZtPnpRjSv0KkbrzhGyT2bo7ZEocFWepmCJftCxNWRk5L3thHmUIibFBM7_x2Lh=w545-h541-no?authuser=0",
+      imageSrc: farm,
       url: "https://github.com/Areskyb/Virtual_Garden_project",
     },
     {
       id: 3, //DO NOT CHANGE THIS (Please)😅
       title: "Kanban Clone",
       service: "Simple Kanban Clone(Angular 8,Firestore,Auth)",
-      imageSrc:
-        "https://lh3.googleusercontent.com/wALW2qB7gOLp_ESOYdNdGGMiAdj3ZYCK66sZsH2zFnT81bPQMoEE1TICnQ-gfh1L1FLJIgggmYWF8f9KQQw8GSzs-bmJ9tMA2NIfqvj3sPrhNeEPQlMbUTVVuzSuyM9XQy9KcvQVrU-l0HXgacHK3dWu39G7S6sgqJyTMGg6-fjJMmh3OvJ0dmVhMmOv6fdpVnsOceUMBTWE3P9ZZITHutM53DlPP9ptDdftIL6acjmIYPq7pm8rs2Tio9J-nCBE34cWJDMv9Q39ODQJ8jSbAOhudZeqzhbI8OoPOZzydjGFIOLFrRvyELC7iS10hBeETtS0hmqN_xarNl7SSTgfkX597KGIFGuOx4vkJ6z0Au4eBypLzAC16zH28RMEbYg_eQEdLZEdIYMtHwJG5zQH74amXeZXcqoWPauw3yFGkEpjatD2oDwqhGFSPcYk5vIEGN8se2sA2KUFlS73NRK3kO-3bMvj0MX_u1VHtauX6aM0T8I63aE3uaPGfPd2zs8LdxjRCBz2oMq_ZBCoj1_XY38L4ooqUViceIxe-umCc5fY5cMBD6exrpqvh1k4ktqnxT-LT3u3UEyykdMVMLJIbwXPGlfXK05rSyMjLqsSCeQuhGGOXaZL25yXMApYxwrnABxJoK5r3aCeSSYa-IIPn9tfYbsuLJOmxcBnje3rwRv8YJKz42LaUGyiq5Pg=s474-no?authuser=0",
+      imageSrc: kanban,
       url: "https://github.com/Areskyb/Trello_angular_firebase",
     },
     {
       id: 4, //DO NOT CHANGE THIS (Please)😅
       title: "The Lazy Nomad",
       service: "First HND Solo project (Ruby)",
-      imageSrc:
-        "https://lh3.googleusercontent.com/AfdZYXDiBdI5TfJ1oh0SG8Q4WUsMU5JfGbS78063z66fkjzWILTxN3FNtgQ0E9BdnNcHCMUsaEwnQKfZIIaLLaylR5t3GpBJYwbKM4kp7XpYdxGr-3SaXWPkpKTLameQDEd3gpBnr1URI6NgwkkHwljuBUsKByUMlG9dgCiuU2c7r8ZsBhxouxv4l15q8WkBaEf2FpgGuiu4TXi77nFlIo5OntIx1u6DFhhgcOWQvsrZb-Yx_QdhB-b23386BRkz7XfLwWfbtukKRNUDpyMfB-krq6AlH4GqxYL8YEd3Xw8Qylrn95_5ZKgFpZUyQsuAoytOsa-SCMG9GGV9xMAp1jepg5pVQ6TvE4MKhVDpUlcadFMq7_Q06Nv98B81D3UZAO-tvdXapYr-aMMUMaM8z4yyPtANYyfw48iQcTpcjxSXnwTVrXXCDCo0RNp9ePFsm3MhFf6MEnO7uYfF9XvCLsQHgup7lD5lyFzfQRb-jVkLc1pu5dtUhmldbp3ihdDS8GC5_kiiN-rk_SYqHGcuATmHlUzvh0ARv1fvFI8wtLlaKol-nwbMBYi8GW7OMx7qPHOaN2zSr4m9PL4JuyYhzYpvTBOOQpkSnKZLukbRD26Xmj2fhSOQ4B7KcOV6y3_FAcnVYktoQK5Vo6DoicYWj2U_EdMy84d-UpM121gG5B4AiFewJyGTbTzxAkQp=s479-no?authuser=0",
+      imageSrc: bag,
       url: "https://github.com/Areskyb/ruby_project",
     },
 
